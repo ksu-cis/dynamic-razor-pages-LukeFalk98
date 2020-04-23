a3ef434ff9ed4cdc2f4a8d8cb064ca1325d2c6a5
Luke Falk <Lukie.babookie@gmail.com>
2020-04-22 20:41:54 -0500

    added search bar and filters

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,10 +9,67 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// the list of movies
+        /// </summary>
+        public IEnumerable<Movie> Movies { get; protected set; }
 
-        public void OnGet()
+        /// <summary>
+        /// The current search terms 
+        /// </summary>
+        [BindProperty]
+        public string SearchTerms { get; set; } = "";
+
+        /// <summary>
+        /// The filtered MPAA Ratings
+        /// </summary>
+        [BindProperty]
+        public string[] MPAARatings { get; set; }
+
+        /// <summary>
+        /// The filtered genres
+        /// </summary>
+        [BindProperty]
+        public string[] Genres { get; set; }
+
+        /// <summary>
+        /// The minimum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMin { get; set; }
+
+        /// <summary>
+        /// The maximum IMDB Rating
+        /// </summary>
+        [BindProperty]
+        public double? IMDBMax { get; set; }
+
+        /// <summary>
+        /// The minimum Rotten Tomatoes Rating
+        /// </summary>
+        [BindProperty]
+        public double? RottenMin { get; set; }
+
+        /// <summary>
+        /// The maximum Rotten Tomatoes Rating
+        /// </summary>
+        [BindProperty]
+        public double? RottenMax { get; set; }
+
+        public void OnGet(double? IMDBMin, double? IMDBMax, double? RottenMin, double? RottenMax, string SearchTerms, string[] MPAARatings, string[] Genres)
         {
-
+            this.RottenMax = RottenMax;
+            this.RottenMin = RottenMin;
+            this.IMDBMin = IMDBMin;
+            this.IMDBMax = IMDBMax;
+            this.MPAARatings = MPAARatings;
+            this.Genres = Genres;
+            this.SearchTerms = SearchTerms;
+            Movies = MovieDatabase.Search(SearchTerms);
+            Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
+            Movies = MovieDatabase.FilterByGenre(Movies, Genres);
+            Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
+            Movies = MovieDatabase.FilterByRottenRating(Movies, RottenMin, RottenMax);
         }
     }
 }
